613c891bbaa6ad3392fad1d499b62666e354c424
rintujrajan <rintujrajan90@gmail.com>
2020-07-12 13:26:00 +0530

    Cleanup of comments

--- StrategyPattern/StrategyPattern.cpp
+++ StrategyPattern/StrategyPattern.cpp
@@ -4,14 +4,6 @@
 #include <iostream>
 
 /******************************************************************************
-
-							  Online C++ Compiler.
-			   Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-/******************************************************************************
 *******************************************************************************
 *******************************************************************************
 The code for this pattern is based up on the Head First Design patterns book
@@ -68,15 +60,3 @@ int main()
 
 	return 0;
 }
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
